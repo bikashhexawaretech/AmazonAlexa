fca1c7e46d4f1a103dc1ec8263554a2784315c80
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-08 11:33:48 +0530

    sdd

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ function buildResponse(title, output, repromptText, shouldEndSessionValue) {
 function Greetings(intent, session, callback) {
     const sessionAttributes = {},
         cardTitle = 'Hey There',
-        speechOutput = 'Hey There I am alexa bot!!!',
-        repromptText = 'Hey There I am alexa bot!!!',
+        speechOutput = 'Hey There I am servicenow assistant.I can help you with create an incident and get the status of an incident',
+        repromptText = 'Hey There I am servicenow assistant.I can help you with create an incident and get the status of an incident',
         shouldEndSession = true;
     callback(sessionAttributes, buildResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
 }
