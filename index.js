2095525b5fcd2db5158cb4858f66284e40caa6b5
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-08 09:54:03 +0530

    sdf

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ app.listen(portC, function(){
     console.log('AGENT is running my app on  PORT: ' + portC);
 });
 
+function Greetings(intent, session, callback) {
+    const sessionAttributes = {},
+        cardTitle = 'Hey There',
+        speechOutput = 'Hey There.I am alexa bot!!!',
+        repromptText = 'Hey There.I am alexa bot!!!',
+        shouldEndSession = true;
+    callback(sessionAttributes, buildResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
 
+function callIntent(req, session, callback){
+	if(req.intent.name === 'Greeting'){
+        console.log('Greeting Intent Called');
+        Greetings(intent, session, callback);
+    }  
+}
+
+function sendResponse(res, attributes, response){
+	return res.json({
+		version: '1.0',
+		attributes,
+		response: response
+	});
+}
 
 app.post('/', (req, res) => {
     
@@ -22,6 +44,14 @@ app.post('/', (req, res) => {
     */
     
     console.log(req.body.request.type);
-     
+    if (req.body.request.type === 'IntentRequest') {
+        callIntent(req.body.request,req.body.session,
+            (attributes, response) => {
+                sendResponse(res, attributes, response);
+            });
+        }
+        else if (req.body.request.type === 'SessionEndedRequest') {
+            console.log("session end");
+        }
 	
 });
